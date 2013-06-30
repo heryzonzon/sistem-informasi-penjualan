fd3c79d14c92e29ec02d6c464f21e8687a1ff7dd
Pewh Gosh <25minutes.again@gmail.com>
2013-06-30 16:02:33 +0700

    Reorganizing CSS & JS

---
+++ static/js/app.js
@@ -0,0 +1,36 @@
+var url = window.location.href;
+
+var addHighlightClass = [
+    { 'url': '/items', 'class': 'item' },
+    { 'url': '/suppliers', 'class': 'supplier' },
+    { 'url': '/customers', 'class': 'customer' },
+    { 'url': '/invoices/purchase', 'class': 'invoice_purchase' },
+    { 'url': '/invoices/sales/', 'class': 'invoice_sales' },
+    { 'url': '/transactions/purchase', 'class': 'transaction_purchase' },
+    { 'url': '/transactions/sales/', 'class': 'transaction_sales' },
+    { 'url': '/users', 'class': 'users' },
+    { 'url': '/login', 'class': 'login' }
+];
+
+for ( var i = 0; i < addHighlightClass.length; i++ ) {
+    var obj = addHighlightClass[i];
+
+    if ( url.match( obj.url ) ) {
+        var menu = document.getElementsByClassName(obj.class)[0];
+        window.tes = menu;
+
+        if ( menu ) {
+            menu.className += ' active';
+            break;
+        }
+    }
+}
+
+// select2
+$('form input, form select').addClass('span3');
+$('form select').select2();
+$('.select2-container').css({
+    marginLeft: 0,
+    paddingLeft: 0
+});
+$('form input[type="checkbox"]').removeClass('span3');
